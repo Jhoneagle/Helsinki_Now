d2d94789ff853017a3eb746701576a5e0c518503
Jhoneagle <eaglekjohn@gmail.com>
2019-05-18 19:46:36 +0300

    places other parts but weekly times ready

--- src/views/SinglePlaceView.js
+++ src/views/SinglePlaceView.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {Card, CardItem, Container, Content, Text} from 'native-base';
+import {Badge, Card, CardItem, Container, Content, Right, Text} from 'native-base';
 import {Linking, StyleSheet} from 'react-native';
-import {cleanHTML, parseTimeStamp} from "../utils";
+import {checkUrl, cleanHTML, hoursFromStringToInt, parseTimeStamp} from "../utils";
 
 export default class SinglePlaceView extends Component {
   render() {
     const object = this.props.value;
+    const date = new Date();
+    let dayNumber = date.getDay();
+    if (dayNumber > 0) {
+      dayNumber--;
+    } else {
+      dayNumber = 6;
+    }
     
     return (
       <Container>
@@ -16,9 +23,31 @@ export default class SinglePlaceView extends Component {
           <Card>
             <CardItem header>
               <Text style={ styles.h3 }>
-                Todo
+                Description
               </Text>
             </CardItem>
+    
+            {object.info_url == null ? <CardItem/> :
+              <CardItem>
+                <Text>
+                  {object.description.intro}
+                </Text>
+              </CardItem>
+            }
+    
+            <CardItem>
+              <Text>
+                {cleanHTML(object.description.body)}
+              </Text>
+            </CardItem>
+    
+            {object.info_url == null ? <CardItem/> :
+              <CardItem footer>
+                <Text style={ styles.link }>
+                  For more <Text style={{color: 'blue'}} onPress={() => Linking.openURL(checkUrl(object.info_url))}>information</Text>.
+                </Text>
+              </CardItem>
+            }
           </Card>
           
           <Card>
@@ -27,6 +56,7 @@ export default class SinglePlaceView extends Component {
                 Location
               </Text>
             </CardItem>
+            
             {object.location.address.street_address == null ? <CardItem/> :
               <CardItem>
                 <Text style={ styles.address }>
@@ -34,6 +64,56 @@ export default class SinglePlaceView extends Component {
                 </Text>
               </CardItem>
             }
+          </Card>
+  
+          <Card>
+            <CardItem header>
+              <Text style={ styles.h3 }>
+                Today
+              </Text>
+            </CardItem>
+  
+            {object.opening_hours.hours == null ?
+              <CardItem><Text>No info available!</Text></CardItem> :
+              (
+                <CardItem>
+                  <Text style={ styles.time }>
+                    {object.opening_hours.hours[dayNumber].open24h ? 'Open hole day!' :
+                      (hoursFromStringToInt(object.opening_hours.hours[dayNumber].opens) == null ? 'Closed hole day' :
+                          'Open: ' + hoursFromStringToInt(object.opening_hours.hours[dayNumber].opens) + '-'
+                          + hoursFromStringToInt(object.opening_hours.hours[dayNumber].closes)
+                      )
+                    }
+                  </Text>
+                  <Right>
+                    {hoursFromStringToInt(object.opening_hours.hours[dayNumber].opens) == null ?
+                      <Badge danger>
+                        <Text>Closed</Text>
+                      </Badge> :
+                      (hoursFromStringToInt(object.opening_hours.hours[dayNumber].opens) <= date.getHours() &&
+                        hoursFromStringToInt(object.opening_hours.hours[dayNumber].closes) > date.getHours() ?
+                          <Badge success>
+                            <Text>Open</Text>
+                          </Badge> :
+                          <Badge danger>
+                            <Text>Closed</Text>
+                          </Badge>
+                      )
+                    }
+                  </Right>
+                </CardItem>
+              )
+            }
+          </Card>
+  
+          <Card>
+            <CardItem header>
+              <Text style={ styles.h3 }>
+                This week
+              </Text>
+            </CardItem>
+    
+            
           </Card>
         </Content>
       </Container>
